e0d4a08177b5f14e22429996e2f59a51f2ad0754
Kavin Mistry <73570657+KavinMistry@users.noreply.github.com>
2022-07-16 12:03:23 +0530

    Update General_purpose_calc.java

--- General_purpose_calc.java
+++ General_purpose_calc.java
@@ -91,6 +91,7 @@ public class General_purpose_calc
                                             System.out.println(" > Enter the value of the second term ");
                                             t2 = sc.nextDouble();
                                             a1 = t1 + t2;
+                                            System.out.println(" Answer is --> " + a1);
                                 break;
                                 case '3' :  double t3;
                                             System.out.println(" > Enter the value of the first term ");
@@ -100,6 +101,7 @@ public class General_purpose_calc
                                             System.out.println(" > Enter the value of the third term ");
                                             t3 = sc.nextDouble();
                                             a1 = t1 + t2 + t3;
+                                            System.out.println(" Answer is --> " + a1);
                                 break;
                                 case '4' :  double t4;
                                             System.out.println(" > Enter the value of the first term ");
@@ -111,6 +113,7 @@ public class General_purpose_calc
                                             System.out.println(" > Enter the value of the fourth term ");
                                             t4 = sc.nextDouble();
                                             a1 = t1 + t2 + t3 + t4;
+                                            System.out.println(" Answer is --> " + a1);
                                 break;
                                 case '5' :  double t5;
                                             System.out.println(" > Enter the value of the first term ");
@@ -124,6 +127,7 @@ public class General_purpose_calc
                                             System.out.println(" > Enter the value of the fifth term ");
                                             t5 = sc.nextDouble();
                                             a1 = t1 + t2 + t3 + t4 + t5;
+                                            System.out.println(" Answer is --> " + a1);
                                 break;
                                 default : System.out.println(" ERROR --> \n> You might have entered a character instead of a number \n> Or you might have entered a number greater than 5 \n> Please note 6 or more terms are not supported yet !");
                 }
